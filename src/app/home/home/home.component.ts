bfe6568030070346674ceefdf106cdfe6654c36b
Daniel <darknessbg@windowslive.com>
2020-05-19 14:12:17 +0300

    show hard coded services inside the home component

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserServicesService } from "../user-services.service";
 
+interface Services {
+  title: string;
+  description: string;
+  imageUrl: string;
+}
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  services: Services[];
 
-  constructor() { }
+  constructor(private uServices: UserServicesService) {}
 
   ngOnInit(): void {
+    this.uServices.getServices().subscribe((services) => {
+      this.services = services;
+    });
   }
-
 }
