40462f43007e32cd6dae6ca8092dabecf3e0e771
Daniel <darknessbg@windowslive.com>
2020-05-19 21:40:12 +0300

    show logout button when user logged in

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  isLoggedIn: boolean;
 
-  constructor() { }
+  constructor(private auth: AuthService) {}
 
   ngOnInit(): void {
+    this.isLoggedIn = this.auth.isAuthenticated();
   }
 
+  handleLogOut() {}
 }
