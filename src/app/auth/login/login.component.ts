7cbbda032575bcc4a7cc8250475e74269161d01b
Daniel <darknessbg@windowslive.com>
2020-05-19 22:46:10 +0300

    login form created, login with registered account

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private fAuth: AngularFireAuth,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.pattern(/\S+@\S+\.\S+/)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
 
+  handleLogin() {
+    if (this.loginForm.valid) {
+      const email = this.loginForm.get('email').value;
+      const password = this.loginForm.get('password').value;
+
+      this.fAuth.signInWithEmailAndPassword(email, password).then((x) => {
+        this.router.navigate(['']);
+      });
+    }
+  }
 }
