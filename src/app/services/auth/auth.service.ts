c34bf01e1848ddb8a7c79f1289ea55b35460ceb3
Daniel <darknessbg@windowslive.com>
2020-05-19 22:47:00 +0300

    changed how auth service works, it returns an observable now

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,34 +1,30 @@
 import { Injectable, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { Observable } from 'rxjs';
-
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService implements OnInit {
   private user$: Observable<firebase.User> = null;
-  private userDetails: firebase.User = null;
+  // private userDetails: firebase.User = null;
 
-  constructor(private afAuth: AngularFireAuth) {
-    this.user$ = this.afAuth.authState;
-
-    this.user$.subscribe((user) => {
-      if (user) {
-        console.log(user.uid);
-        this.userDetails = user;
-      } else {
-        this.userDetails = null;
-      }
-    });
+  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore) {
+    this.user$ = this.afAuth.authState.pipe(
+      switchMap((user) => {
+        if (user) {
+          return this.afs.doc<any>(`profiles/${user.uid}`).valueChanges();
+        } else {
+          return of(null);
+        }
+      })
+    );
   }
 
   ngOnInit() {}
 
-  isAuthenticated() {
-    if (this.userDetails === null) {
-      return false;
-    } else {
-      return true;
-    }
+  getUser() {
+    return this.user$;
   }
 }
