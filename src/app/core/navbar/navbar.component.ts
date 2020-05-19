323b9c824165d1419fa852996b53b72a4b9f8ab6
Daniel <darknessbg@windowslive.com>
2020-05-19 22:47:40 +0300

    navbar shows different navlinks depends if user is logged in or not

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-navbar',
@@ -9,11 +10,19 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 export class NavbarComponent implements OnInit {
   isLoggedIn: boolean;
 
-  constructor(private auth: AuthService) {}
-
-  ngOnInit(): void {
-    this.isLoggedIn = this.auth.isAuthenticated();
+  constructor(private auth: AuthService, private fAuth: AngularFireAuth) {
+    this.auth.getUser().subscribe((user) => {
+      if (user === null) {
+        this.isLoggedIn = false;
+      } else {
+        this.isLoggedIn = true;
+      }
+    });
   }
 
-  handleLogOut() {}
+  ngOnInit(): void {}
+
+  handleLogOut() {
+    this.fAuth.signOut();
+  }
 }
