c12c4c0e25a587a8d9c79b9ba26b082496923a6f
Daniel <darknessbg@windowslive.com>
2020-05-19 11:54:03 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AngularFireModule } from "@angular/fire";
+import { environment } from "../environments/environment";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA4KfKl8e_e4fbWKLU-CKaSCNOUiz26mAY",
+    authDomain: "connectvalve.firebaseapp.com",
+    databaseURL: "https://connectvalve.firebaseio.com",
+    projectId: "connectvalve",
+    storageBucket: "connectvalve.appspot.com",
+    messagingSenderId: "1077261852366",
+    appId: "1:1077261852366:web:549d8167ac85596be2ba9f",
+    measurementId: "G-NGQ9111VEJ",
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
