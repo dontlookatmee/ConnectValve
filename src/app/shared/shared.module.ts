71b3399e1d07541003ea263f8f8064aa25630ed4
Daniel <darknessbg@windowslive.com>
2020-05-19 13:40:06 +0300

    added service cards in home template

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ServiceCardComponent } from "./service-card/service-card.component";
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ServiceCardComponent],
+  imports: [CommonModule],
+  exports: [ServiceCardComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
