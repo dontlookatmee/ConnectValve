177d8bc41ec546240c8437bd94b86bd465d52be4
Daniel <darknessbg@windowslive.com>
2020-05-19 12:48:37 +0300

    home component template changes, added illustration

---
+++ src/app/home/home.routing.module.ts
@@ -0,0 +1,11 @@
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+  },
+];
+
+export const HomeRoutingModule = RouterModule.forChild(routes);
