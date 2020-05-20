1d56cecce9c619d855049e6e7631cb3fba355ff7
Daniel <darknessbg@windowslive.com>
2020-05-20 15:58:45 +0300

    navbar component clean up

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -2,30 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { AngularFireAuth } from '@angular/fire/auth';
 
-interface User {
-  avatar?: string;
-  description?: string;
-  email?: string;
-  last_changed?: {
-    seconds?: number;
-    nanoseconds?: number;
-  };
-  name?: string;
-  password?: string;
-  services?: string[];
-  status?: string;
-  uid?: string;
-}
-
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-  isLoggedIn: boolean;
-  user: User;
-
   constructor(public auth: AuthService, private fAuth: AngularFireAuth) {}
 
   ngOnInit(): void {}
