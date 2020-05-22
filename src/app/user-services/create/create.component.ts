ead4521b1a9873051386b9318e8e97554d8ce0b7
Daniel <darknessbg@windowslive.com>
2020-05-22 19:42:29 +0300

    init create component for user services

---
+++ src/app/user-services/create/create.component.ts
@@ -0,0 +1,99 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { isValueNumber } from '../../custom-validators/isNumber';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { UserServicesService } from 'src/app/services/user-services/user-services.service';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.css'],
+})
+export class CreateComponent implements OnInit {
+  createForm: FormGroup;
+  feedback: { visible?: boolean; status?: string; msg?: string };
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private userServices: UserServicesService
+  ) {}
+
+  ngOnInit(): void {
+    this.createForm = this.fb.group({
+      category: ['', [Validators.required]],
+      title: ['', [Validators.required, Validators.maxLength(35)]],
+      description: ['', [Validators.required, Validators.maxLength(350)]],
+      image: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
+          ),
+        ],
+      ],
+      price: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
+    });
+  }
+
+  handleServiceCreate() {
+    const uid = this.authService.getUserId();
+    const category = this.createForm.get('category').value;
+    const title = this.createForm.get('title').value;
+    const description = this.createForm.get('description').value;
+    const image = this.createForm.get('image').value;
+    const price = parseFloat(this.createForm.get('price').value);
+
+    this.authService.user$.subscribe((user) => {
+      const data = {
+        category,
+        title,
+        description,
+        image,
+        price,
+        uid,
+        name: user.name,
+      };
+
+      this.userServices
+        .createService(data)
+        .then((x) => {
+          this.feedback = this.showPopupMsg(
+            true,
+            'success',
+            'Service created successful!'
+          );
+          setTimeout(() => {
+            this.feedback = this.showPopupMsg(false);
+          }, 2500);
+
+          this.createForm.reset();
+        })
+        .catch((err) => {
+          this.feedback = this.showPopupMsg(
+            true,
+            'error',
+            'Something went wrong! Please try again.'
+          );
+          setTimeout(() => {
+            this.feedback = this.showPopupMsg(false);
+          }, 3000);
+        });
+    });
+  }
+
+  handleCancelCreate() {
+    this.createForm.reset();
+  }
+
+  showPopupMsg(visible: boolean, status?: string, msg?: string): {} {
+    const data = {
+      visible,
+      status,
+      msg,
+    };
+
+    return data;
+  }
+}
