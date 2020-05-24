1039930b04c8dc8518762e8589435798b6e7eaf8
Daniel <darknessbg@windowslive.com>
2020-05-24 13:24:56 +0300

    created form group for my-service edit mode

--- src/app/user-services/my-services/my-service/my-service.component.ts
+++ src/app/user-services/my-services/my-service/my-service.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-my-service',
@@ -10,9 +11,47 @@ export class MyServiceComponent implements OnInit {
   @Input('description') description: string;
   @Input('image') image: string;
   @Input('category') category: string;
+  @Input('price') price: string;
   @Input('id') id: string;
 
-  constructor() {}
+  editForm: FormGroup;
+  editMode: boolean = false;
 
-  ngOnInit(): void {}
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.editForm = this.fb.group({
+      category: ['', [Validators.required]],
+      title: ['', [Validators.required, Validators.maxLength(35)]],
+      description: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(20),
+          Validators.maxLength(350),
+        ],
+      ],
+      image: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
+          ),
+        ],
+      ],
+      price: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
+    });
+  }
+
+  handleServiceEdit() {}
+
+  handleCancelEdit() {
+    this.editForm.reset();
+    this.editMode = false;
+  }
+
+  handleEdit() {
+    this.editMode = true;
+  }
 }
