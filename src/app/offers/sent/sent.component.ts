b9c604a3faea2c602cfb37b9bb1f9706ae531eb0
Daniel <darknessbg@windowslive.com>
2020-05-24 10:24:28 +0300

    successful fetch sent offers and service component image onerror handle

--- src/app/offers/sent/sent.component.ts
+++ src/app/offers/sent/sent.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { OffersService } from 'src/app/services/offers/offers.service';
 
+interface Offer {
+  id: string;
+  data: {
+    collaborationCreated: boolean;
+    fromUser: string;
+    note: string;
+    price: number;
+    service: string;
+    status: string;
+    time: number;
+    toUser: string;
+  };
+}
 @Component({
   selector: 'app-sent',
   templateUrl: './sent.component.html',
-  styleUrls: ['./sent.component.css']
+  styleUrls: ['./sent.component.css'],
 })
 export class SentComponent implements OnInit {
+  offersSent: Offer[];
 
-  constructor() { }
+  constructor(private offersService: OffersService) {}
 
   ngOnInit(): void {
+    this.offersService.getReceivedOffers().subscribe((offers: Offer[]) => {
+      this.offersSent = offers;
+    });
   }
-
 }
