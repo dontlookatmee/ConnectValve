1e847d89d910f4dce1cf93a20d7e61fc5d617706
Daniel <darknessbg@windowslive.com>
2020-05-29 12:22:40 +0300

    show status and replied time on recived collaborations

--- src/app/collaborations/collaboration/collaboration.component.ts
+++ src/app/collaborations/collaboration/collaboration.component.ts
@@ -5,8 +5,14 @@ import {
   Collaboration,
 } from 'src/app/services/collaboration/collaboration.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
-import { switchMap, mergeMap } from 'rxjs/operators';
-import { Subscription, of } from 'rxjs';
+import { switchMap, mergeMap, take } from 'rxjs/operators';
+import { of } from 'rxjs';
+
+interface Messages {
+  name: string;
+  message: string;
+  date: Date;
+}
 
 @Component({
   selector: 'app-collaboration',
@@ -19,7 +25,7 @@ export class CollaborationComponent implements OnInit {
   @Input('status') status: string;
   @Input('img') img: string;
   @Input('id') id: string;
-  updateCb: Subscription;
+  lastRep: string;
 
   constructor(
     private router: Router,
@@ -27,12 +33,22 @@ export class CollaborationComponent implements OnInit {
     private auth: AuthService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.cbService
+      .getCollaborationMessages(this.id)
+      .subscribe((cb: Messages[]) => {
+        const timeNow = Number(Date.now());
+        const lastRep = Number(cb[cb.length - 1].date);
+
+        this.lastRep = this.calcTime(timeNow, lastRep);
+      });
+  }
 
   handleEnterCb() {
-    this.updateCb = this.cbService
+    this.cbService
       .getCollaboration(this.id)
       .pipe(
+        take(1),
         switchMap((collaboration: Collaboration) => {
           const toUser = collaboration.data.toUser;
           return of(toUser);
@@ -50,7 +66,29 @@ export class CollaborationComponent implements OnInit {
     // });
   }
 
-  ngOnDestroy() {
-    this.updateCb?.unsubscribe();
+  calcTime(current: number, previous: number) {
+    let msPerMinute = 60 * 1000;
+    let msPerHour = msPerMinute * 60;
+    let msPerDay = msPerHour * 24;
+    let msPerMonth = msPerDay * 30;
+    let msPerYear = msPerDay * 365;
+
+    let elapsed = current - previous;
+
+    if (elapsed < msPerMinute) {
+      return Math.round(elapsed / 1000) + ' seconds ago';
+    } else if (elapsed < msPerHour) {
+      return Math.round(elapsed / msPerMinute) + ' minutes ago';
+    } else if (elapsed < msPerDay) {
+      return Math.round(elapsed / msPerHour) + ' hours ago';
+    } else if (elapsed < msPerMonth) {
+      return 'approximately ' + Math.round(elapsed / msPerDay) + ' days ago';
+    } else if (elapsed < msPerYear) {
+      return (
+        'approximately ' + Math.round(elapsed / msPerMonth) + ' months ago'
+      );
+    } else {
+      return 'approximately ' + Math.round(elapsed / msPerYear) + ' years ago';
+    }
   }
 }
