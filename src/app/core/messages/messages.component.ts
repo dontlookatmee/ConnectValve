63c9eaa60de7352c2820b2cc5c6660d87556adb8
Daniel <darknessbg@windowslive.com>
2020-05-31 14:51:01 +0300

    created messages component for messages navbar link

---
+++ src/app/core/messages/messages.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  Collaboration,
+  CollaborationService,
+} from 'src/app/services/collaboration/collaboration.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css'],
+})
+export class MessagesComponent implements OnInit {
+  myCollaborations: Collaboration[];
+
+  constructor(
+    public auth: AuthService,
+    private authService: AuthService,
+    private cbService: CollaborationService
+  ) {}
+
+  ngOnInit(): void {
+    const userId = this.authService.getUserId();
+    console.log(userId);
+    this.cbService
+      .getMyCollaborations(userId)
+      .subscribe((collaborations: Collaboration[]) => {
+        this.myCollaborations = collaborations.filter(
+          (cb: Collaboration) => cb.data.status === 'pending'
+        );
+      });
+  }
+}
