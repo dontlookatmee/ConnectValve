ecf5d6dd4d21ce1b0df97cfa8ca5dace8f71f26f
Daniel <darknessbg@windowslive.com>
2020-06-02 18:39:50 +0300

    created user card component, added temporary css

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
+  @HostBinding('style.margin')
+  margin: string = '0 auto';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
