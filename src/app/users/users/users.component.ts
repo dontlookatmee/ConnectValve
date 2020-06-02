7e0b4d72697d09b8a956ba2ee7b0d5d20ab9a294
Daniel <darknessbg@windowslive.com>
2020-06-02 18:45:57 +0300

    fetch all user profiles inside users component

--- src/app/users/users/users.component.ts
+++ src/app/users/users/users.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  ProfileService,
+  UsersMeta,
+} from 'src/app/services/profile/profile.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
+  users: UsersMeta[];
 
-  constructor() { }
+  constructor(private profileService: ProfileService) {}
 
   ngOnInit(): void {
+    this.profileService.getUsersProfiles().subscribe((users: UsersMeta[]) => {
+      this.users = users;
+    });
   }
-
 }
