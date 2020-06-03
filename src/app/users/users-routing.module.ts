2df56d7cb1f05d7ae3bd84d997b6034efe9926c9
Daniel <darknessbg@windowslive.com>
2020-06-03 14:12:41 +0300

    added route for user-profile component

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,10 +1,15 @@
 import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent } from './users/users.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes = [
   {
     path: '',
     component: UsersComponent,
+  },
+  {
+    path: ':id',
+    component: UserProfileComponent,
   },
 ];
 
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users/users.component';
 import { UserCardComponent } from './user-card/user-card.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { ProfileServiceCardComponent } from './profile-service-card/profile-service-card.component';
 
 @NgModule({
-  declarations: [UsersComponent, UserCardComponent],
+  declarations: [UsersComponent, UserCardComponent, UserProfileComponent, ProfileServiceCardComponent],
   imports: [CommonModule, UsersRoutingModule],
 })
 export class UsersModule {}
