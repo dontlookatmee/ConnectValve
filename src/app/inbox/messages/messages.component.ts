9948b64ff140c5aa0de2f00b64a4e4af656a2d49
Daniel <darknessbg@windowslive.com>
2020-06-04 14:16:31 +0300

    fetch user messages inside messages component

--- src/app/inbox/messages/messages.component.ts
+++ src/app/inbox/messages/messages.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from 'src/app/services/profile/profile.service';
 
+interface Messages {
+  id: string;
+  data: {
+    fromUser: string;
+    fromUserAvatar: string;
+    fromUserId: string;
+    date: number;
+    subject: string;
+    message: string;
+  };
+}
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  styleUrls: ['./messages.component.css'],
 })
 export class MessagesComponent implements OnInit {
+  messages: Messages[];
 
-  constructor() { }
+  constructor(private profileService: ProfileService) {}
 
   ngOnInit(): void {
+    this.profileService.getUserMessages().subscribe((messages: Messages[]) => {
+      this.messages = messages;
+    });
   }
-
 }
