59530858331fae1f4e298cddcf2d059a5fa20565
Daniel <darknessbg@windowslive.com>
2020-06-04 19:28:00 +0300

    created messages component

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { InboxRoutingModule } from './inbox-routing.module';
 import { MessagesComponent } from './messages/messages.component';
+import { MessageComponent } from './message/message.component';
 
 
 @NgModule({
-  declarations: [MessagesComponent],
+  declarations: [MessagesComponent, MessageComponent],
   imports: [
     CommonModule,
     InboxRoutingModule
