3bbc28d864fb4c373ad1d054bcdd8f185b0593e0
Daniel <darknessbg@windowslive.com>
2020-06-05 11:56:15 +0300

    messages component now fetch messages and send data to message comp

--- src/app/inbox/messages/messages.component.ts
+++ src/app/inbox/messages/messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ProfileService } from 'src/app/services/profile/profile.service';
 
 interface Messages {
@@ -16,6 +16,7 @@ interface Messages {
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class MessagesComponent implements OnInit {
   messages: Messages[];
@@ -24,7 +25,8 @@ export class MessagesComponent implements OnInit {
 
   ngOnInit(): void {
     this.profileService.getUserMessages().subscribe((messages: Messages[]) => {
-      this.messages = messages;
+      this.messages = messages.sort((a, b) => b.data.date - a.data.date);
+      console.log(messages);
     });
   }
 }
