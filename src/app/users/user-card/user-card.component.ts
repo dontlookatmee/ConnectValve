ae676b26cac27601ceafcb6d3bddb78faec15699
Daniel <darknessbg@windowslive.com>
2020-06-06 16:59:25 +0300

    user can vote for another user

--- src/app/users/user-card/user-card.component.ts
+++ src/app/users/user-card/user-card.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, HostBinding, Input } from '@angular/core';
+import { ProfileService, User } from 'src/app/services/profile/profile.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-card',
@@ -15,7 +18,37 @@ export class UserCardComponent implements OnInit {
   @Input('status') status: string;
   @Input('userId') userId: string;
   @Input('votes') votes: string[];
-  constructor() {}
+  hasVoted: boolean;
+  user: User;
+  loggedUserId: string;
+  constructor(
+    private profileService: ProfileService,
+    private authService: AuthService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loggedUserId = this.authService.getUserId();
+
+    this.profileService
+      .getUserProfile(this.userId)
+      .subscribe((user: User) => (this.user = user));
+  }
+
+  handleUserVote() {
+    this.profileService
+      .getUserProfile(this.userId)
+      .pipe(take(1))
+      .subscribe((user: User) => {
+        if (user.uid === this.loggedUserId) {
+        } else if (user.votes.includes(this.loggedUserId)) {
+          this.profileService.removeVote(this.userId);
+          this.hasVoted = false;
+          console.log(this.hasVoted);
+        } else {
+          this.profileService.addVote(this.userId);
+          this.hasVoted = true;
+          console.log(this.hasVoted);
+        }
+      });
+  }
 }
