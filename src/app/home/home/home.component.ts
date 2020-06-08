3d2a4ae9c430288844c3d963ce63c211cd8a37d1
Daniel <darknessbg@windowslive.com>
2020-06-08 16:13:31 +0300

    changed search field BG color

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { UserServicesService } from '../../services/user-services/user-services.service';
 import { Subscription } from 'rxjs';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { ProfileService } from 'src/app/services/profile/profile.service';
 
 interface Service {
   id: string;
@@ -21,10 +22,18 @@ interface Service {
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  @HostListener('window:beforeunload', ['$event'])
+  unloadHandler(event) {
+    this.profileService.updateUserProfile({ status: 'offline' });
+  }
+
   allServices: Service[];
   servicesSub: Subscription;
 
-  constructor(private uServices: UserServicesService) {}
+  constructor(
+    private uServices: UserServicesService,
+    private profileService: ProfileService
+  ) {}
 
   ngOnInit(): void {
     this.servicesSub = this.uServices
