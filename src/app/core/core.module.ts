fb0d808532a6adee71feb34f2e8b261e24bc2e04
Daniel <darknessbg@windowslive.com>
2020-06-09 18:56:04 +0300

    added footer to homepage

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,19 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { RouterModule } from "@angular/router";
-import { NavbarComponent } from "./navbar/navbar.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 import { MessageComponent } from './message/message.component';
 import { MessagesComponent } from './messages/messages.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [NavbarComponent, MessageComponent, MessagesComponent],
+  declarations: [
+    NavbarComponent,
+    MessageComponent,
+    MessagesComponent,
+    FooterComponent,
+  ],
   imports: [CommonModule, RouterModule],
-  exports: [NavbarComponent],
+  exports: [NavbarComponent, FooterComponent],
 })
 export class CoreModule {}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { HomeComponent } from "./home/home.component";
-import { HomeRoutingModule } from "./home.routing.module";
-import { SharedModule } from "../shared/shared.module";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { HomeRoutingModule } from './home.routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, SharedModule],
+  imports: [CommonModule, HomeRoutingModule, SharedModule, CoreModule],
 })
 export class HomeModule {}
