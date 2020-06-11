88f32aeefd2ca51a8a5e06136b67745e922515f3
Daniel <darknessbg@windowslive.com>
2020-06-11 22:41:46 +0300

    logout user on tab close

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, HostListener } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ConnectValve';
+  @HostListener('window:beforeunload')
+  handleUnload() {
+    this.afAuth.signOut();
+  }
+
+  constructor(private afAuth: AngularFireAuth) {}
+
+  ngOnInit(): void {}
 }
