d311ce394cfac3b4202fa1b67379461362076e5d
Daniel <darknessbg@windowslive.com>
2020-06-25 22:54:50 +0300

    found a bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component, HostListener, Host } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { ProfileService } from './services/profile/profile.service';
+import { from } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(private afAuth: AngularFireAuth) {}
+  constructor(
+    private afAuth: AngularFireAuth,
+    private profileService: ProfileService
+  ) {}
 
   ngOnInit(): void {}
 }
